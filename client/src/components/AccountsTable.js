a7ff5e4b4d489ba540e165b6df1399ebaef772f4
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-02-26 17:35:49 +0800

    Add New Account button

--- client/src/components/AccountsTable.js
+++ client/src/components/AccountsTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, Button, Divider, Icon, Tooltip} from 'antd';
+import { Table, Button, Divider, Icon, Tooltip, Row, Col} from 'antd';
 
 class AccountsTable extends React.Component {
 
@@ -28,10 +28,10 @@ class AccountsTable extends React.Component {
                   </Tooltip>
                   <Divider type="vertical" />
                   <Tooltip title="Edit Account">
-                     <Button><Icon type="profile" /></Button>
+                     <Button><Icon type="form" /></Button>
                   </Tooltip>
                   <Divider type="vertical" />
-                  <Tooltip title="Deleta account">
+                  <Tooltip title="Delete account">
                      <Button type="danger"><Icon type="delete" /></Button>
                   </Tooltip>
                </React.Fragment>
@@ -66,28 +66,47 @@ class AccountsTable extends React.Component {
             role: 'Housekeeper?'
          }
       ];
+
+      const TableTitle = () => {
+         return (
+            <React.Fragment>
+               <Row>
+                  <Col span={12}>
+                     <h1 style={{margin: 0}} >Accounts</h1>
+                  </Col>
+                  <Col align="right" span={12}>
+                     <Button type="primary"><Icon type="usergroup-add" />Add New Account</Button>
+                  </Col>
+               </Row>
+             
+              
+            </React.Fragment>
+         );
+      }
        
       return (
-         <Table
-            columns={columns}
-            dataSource={data}
-            bordered
-            title={() => <h1 style={{margin: 0}}>Accounts</h1>}
-            scroll={{x: 300}}
-            loading={this.state.loading}
-            pagination={
-               {
-                  defaultCurrent: 1,
-                  pageSize: 4,
-                  onChange: (page, pageSize) => {
-                     this.setState({loading: true});
-                     setTimeout(() => {
-                        this.setState({loading: false});
-                     }, 500);
+         <React.Fragment>
+            <Table
+               columns={columns}
+               dataSource={data}
+               bordered
+               title={TableTitle}
+               scroll={{x: 300}}
+               loading={this.state.loading}
+               pagination={
+                  {
+                     defaultCurrent: 1,
+                     pageSize: 4,
+                     onChange: (page, pageSize) => {
+                        this.setState({loading: true});
+                        setTimeout(() => {
+                           this.setState({loading: false});
+                        }, 500);
+                     }
                   }
                }
-            }
-         />
+            />
+         </React.Fragment>
       );
    }
  
