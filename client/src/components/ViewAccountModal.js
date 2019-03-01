9717e5bffb5c9f0af0603a0835d3d9edbe295478
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-03-01 16:55:32 +0800

    Refactor to separate component file

--- client/src/components/ViewAccountModal.js
+++ client/src/components/ViewAccountModal.js
@@ -1,29 +1,7 @@
 import React from 'react';
 import {Modal, Icon, Row, Col, Divider} from 'antd';
+import DescriptionItem from './DescriptionItem';
 import moment from 'moment';
-
-const DescriptionItem = ({ title, content }) => (
-   <div
-     style={{
-       fontSize: 14,
-       lineHeight: '22px',
-       marginBottom: 7,
-       color: 'rgba(0,0,0,0.65)',
-     }}
-   >
-     <p
-       style={{
-         marginRight: 8,
-         display: 'inline-block',
-         color: 'rgba(0,0,0,0.85)',
-         fontWeight: 'bold'
-       }}
-     >
-       {title}:
-     </p>
-     {content}
-   </div>
- );
 
 
 const ViewAccountModal = (props) => {
