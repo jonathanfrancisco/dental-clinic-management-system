447cd464ec0ee1609db0f70c8b030cd145660de6
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-03-02 20:55:48 +0800

    Add witAuth middleware

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,15 @@
 const Router = require('express').Router();
 const userController = require('../controllers/userController');
 const withAuth = require('../services/withAuth');
-Router.get('/', userController.users);
-Router.post('/create', userController.create);
-Router.delete('/delete', userController.delete);
+Router.get('/', withAuth,  userController.users);
+Router.post('/create', withAuth, userController.create);
+Router.delete('/delete', withAuth, userController.delete);
 Router.post('/login', userController.login);
 Router.post('/logout', userController.logout);
 Router.get('/checkToken', withAuth, (req, res) => {
    res.status(200).send({user: req.user});
 });
-Router.get('/:id', userController.getUserById);
+Router.get('/:id', withAuth, userController.getUserById);
 
 
 
