b341be57488a0291cc894955fd4561d064b0e8a6
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-03-04 23:37:00 +0800

    Add  code column/property to patient table

--- migrations/20190304103041_create_patient_table.js
+++ migrations/20190304103041_create_patient_table.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
    return knex.schema.createTable('patient', function(table) {
       table.increments();
+      table.string('code').notNUllable();
       table.string('name').notNullable();
       table.string('address').notNullable();
       table.datetime('birthday').notNullable()
