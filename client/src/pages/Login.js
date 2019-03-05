4cb755b7d3a465f7a4266310fbad8fe1630a01f8
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-03-05 13:14:41 +0800

    Change h to Title component

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Row, Col, Card, Divider} from 'antd';
+import {Row, Col, Card, Divider, Typography} from 'antd';
 import LoginForm from '../components/LoginForm';
 
+const {Title} = Typography;
 
 class Login extends React.Component {
    render() {
@@ -9,11 +10,11 @@ class Login extends React.Component {
          <Row type="flex" align="middle" style={{minHeight: '100vh', background: '#0c162e'}}>
             <Col md={{span: 8, offset: 8}} sm={{span: 16, offset: 4}} xs={{span: 22, offset: 1}}>
                <Card bordered={true}>
-                  <h2 style={{textAlign: 'center'}}>
+                  <Title style={{textAlign: 'center'}} level={2}>
                      Andres Dental Clinic
                      <br /> 
                      Management System
-                  </h2>
+                  </Title>
                   <Divider />
                   <LoginForm handleLogin={this.props.handleLogin} />
                </Card>          
