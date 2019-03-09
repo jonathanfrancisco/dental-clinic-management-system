89272687688ffea70ca3e7f7483c565c7b6fcba6
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-03-09 13:39:58 +0800

    Remove table

--- client/src/components/DentalRecord.js
+++ client/src/components/DentalRecord.js
@@ -63,66 +63,7 @@ class DentalRecord extends React.Component {
    }
 
    render() {
-      const columns = [{
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      render: text => <a href="javascript:;">{text}</a>,
-      }, {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-      }, {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-      }, {
-      title: 'Tags',
-      key: 'tags',
-      dataIndex: 'tags',
-      render: tags => (
-         <span>
-            {tags.map(tag => {
-            let color = tag.length > 5 ? 'geekblue' : 'green';
-            if (tag === 'loser') {
-               color = 'volcano';
-            }
-            return <Tag color={color} key={tag}>{tag.toUpperCase()}</Tag>;
-            })}
-         </span>
-      ),
-      }, {
-      title: 'Action',
-      key: 'action',
-      render: (text, record) => (
-         <span>
-            <a href="javascript:;">Invite {record.name}</a>
-            <Divider type="vertical" />
-            <a href="javascript:;">Delete</a>
-         </span>
-      ),
-      }];
-
-      const data = [{
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
-      }, {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-      tags: ['loser'],
-      }, {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
-      }];
-
+     
       const lastVisit = moment(this.state.patient.last_visit).format('YYYY-MM-DD HH:MM:SS') === '1000-01-01 00:01:00' 
       ? <Tag color="geekblue">New Record</Tag> : moment(this.state.patient.last_visit).format('MMMM, DD YYYY');
       const birthday = moment(this.state.patient.birthday).format('MMMM DD, YYYY');
@@ -163,7 +104,6 @@ class DentalRecord extends React.Component {
                       </TabPane>
                    </Tabs>
                    <Divider orientation="left">Treatments and/or Procedures taken <a><Icon type="plus" /> Add</a></Divider>
-                   <Table columns={columns} dataSource={data} />
                 </Card> 
                )}
          </React.Fragment>
