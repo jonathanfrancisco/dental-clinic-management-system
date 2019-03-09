7978be28e423f50d05ebfe0e8e082c961b759ad7
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-03-09 13:31:24 +0800

    Create migration for patient_adult_teeth_chart and patient_child_teeth_chart

---
+++ migrations/20190309132405_create_patient_child_teeth_chart.js
@@ -0,0 +1,29 @@
+exports.up = function(knex, Promise) {
+   return knex.schema.createTable('patient_child_teeth_chart', function(table) {
+      table.integer('patient_id').unsigned().primary().notNullable();
+      table.string('UR_A').nullable();
+      table.string('UR_B').nullable();
+      table.string('UR_C').nullable();
+      table.string('UR_D').nullable();
+      table.string('UR_E').nullable();
+      table.string('UL_F').nullable();
+      table.string('UL_G').nullable();
+      table.string('UL_H').nullable();
+      table.string('UL_I').nullable();
+      table.string('UL_J').nullable();
+      table.string('LL_K').nullable();
+      table.string('LL_L').nullable();
+      table.string('LL_M').nullable();
+      table.string('LL_N').nullable();
+      table.string('LL_O').nullable();
+      table.string('LR_P').nullable();
+      table.string('LR_Q').nullable();
+      table.string('LR_R').nullable();
+      table.string('LR_S').nullable();
+      table.string('LR_T').nullable();
+   })
+};
+
+exports.down = function(knex, Promise) {
+
+};
