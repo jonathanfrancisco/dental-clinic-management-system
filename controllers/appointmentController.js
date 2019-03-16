a70a2650acd138c78ea891d9c14ef0215741b5e1
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-03-16 18:58:27 +0800

    Change order to ASC

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -12,13 +12,13 @@ module.exports.appointments = async (req, res) => {
          appointments = await Appointment.query().select('appointment.*', 'patient.name')
          .join('patient','patient.id', 'appointment.patient_id')
          .whereBetween('date_time', [req.query.startDate+' 00:00:00', req.query.endDate+' 23:59:59'])
-         .orderBy(raw('UNIX_TIMESTAMP(appointment.date_time)'),'DESC');
+         .orderBy(raw('UNIX_TIMESTAMP(appointment.date_time)'),'ASC');
    
       }
       else
          appointments = await Appointment.query().select('appointment.*', 'patient.name')
                            .join('patient','patient.id', 'appointment.patient_id')
-                           .orderBy(raw('UNIX_TIMESTAMP(date_time)'),'DESC');
+                           .orderBy(raw('UNIX_TIMESTAMP(date_time)'),'ASC');
       
       res.send({appointments});
    } catch(err) {
