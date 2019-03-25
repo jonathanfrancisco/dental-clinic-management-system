22c14d6c0ee79a392c40f02d7ae29f26e7a7c224
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-03-25 22:40:18 +0800

    Change order by to id

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -23,20 +23,21 @@ module.exports.appointments = async (req, res) => {
       let appointments;
       
       if(req.query.startDate && req.query.endDate) {
+         //    .orderBy(raw('UNIX_TIMESTAMP(appointment.date_time)'),'ASC');
 
          if(req.query.search) {
             appointments = await Appointment.query().select('appointment.*', 'patient.name')
             .join('patient','patient.id', 'appointment.patient_id')
             .whereRaw(`LOWER(patient.name) like "%${req.query.search.toLowerCase()}%"`)
             .whereBetween('date_time', [req.query.startDate+' 00:00:00', req.query.endDate+' 23:59:59'])
-            .orderBy(raw('UNIX_TIMESTAMP(appointment.date_time)'),'ASC');
+            .orderBy('appointment.id', "DESC");
          }
 
          else {
             appointments = await Appointment.query().select('appointment.*', 'patient.name')
             .join('patient','patient.id', 'appointment.patient_id')
             .whereBetween('date_time', [req.query.startDate+' 00:00:00', req.query.endDate+' 23:59:59'])
-            .orderBy(raw('UNIX_TIMESTAMP(appointment.date_time)'),'ASC');
+            .orderBy('appointment.id', "DESC");
          }
         
 
@@ -48,13 +49,13 @@ module.exports.appointments = async (req, res) => {
             appointments = await Appointment.query().select('appointment.*', 'patient.name')
             .join('patient','patient.id', 'appointment.patient_id')
             .whereRaw(`LOWER(patient.name) like "%${req.query.search.toLowerCase()}%"`)
-            .orderBy(raw('UNIX_TIMESTAMP(date_time)'),'ASC')
+            .orderBy('appointment.id', "DESC");
             
          }
          else {
             appointments = await Appointment.query().select('appointment.*', 'patient.name')
                               .join('patient','patient.id', 'appointment.patient_id')
-                              .orderBy(raw('UNIX_TIMESTAMP(date_time)'),'ASC');
+                              .orderBy('appointment.id', "DESC");
          }
       }
       
