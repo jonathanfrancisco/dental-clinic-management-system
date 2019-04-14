812ac75985e62df571c69ff8718d9edbbc16333c
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-04-14 16:32:55 +0800

    Use environment variables

--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,10 @@ module.exports = {
    development: {
       client: 'mysql2',
       connection: {
-         host: 'localhost',
-         user: 'root',
-         password: 'mathematics',
-         database: 'dcms',
+         host: process.env.DB_HOST,
+         user: process.env.DB_USER,
+         password: process.env.DB_PASS,
+         database: process.env.DB_NAME,
       }
    },
 
