4ec407c260dc9fdbf4e6b30911da529ca49d909d
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-04-21 16:12:33 +0800

    Add forgot password link

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
    Form, Icon, Input, Button, Modal, Typography
  } from 'antd';
-
  const {Text} = Typography;
  
  class NormalLoginForm extends React.Component {
@@ -35,23 +34,24 @@ import {
      const { getFieldDecorator } = this.props.form;
      return (
          <Form onSubmit={this.handleSubmit} className="login-form">
-            <Text style={{color: 'black'}}>Username</Text>
-            <Form.Item>
+            <Form.Item style={{margin: 0}} label="Username">
                {getFieldDecorator('username', {
                   rules: [{ required: true, message: 'Please input your username!' }],
                })(
                   <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
                )}
             </Form.Item>
-            <Text style={{color: 'black'}}>Password</Text>
-            <Form.Item>
+            <Form.Item style={{margin: 0}} label="Password">
                {getFieldDecorator('password', {
                   rules: [{ required: true, message: 'Please input your password!' }],
                })(
                   <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
                )}
             </Form.Item>
-            <Form.Item>
+            <Form.Item style={{margin: 0, textAlign: 'right'}}>
+               <a href="">Forgot Password?</a>
+            </Form.Item>
+            <Form.Item style={{marginTop: 11}}>
                <Button block type="primary" htmlType="submit" className="login-form-button">
                   <Icon type="login" />
                   Log in
