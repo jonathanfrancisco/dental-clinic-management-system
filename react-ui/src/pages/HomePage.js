94ef89be5eeed86efd5b5711ba46f0adf8c2152c
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2019-04-25 02:59:59 +0800

    Add contents on homepage

--- react-ui/src/pages/HomePage.js
+++ react-ui/src/pages/HomePage.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import { Card, Layout, Menu, Row, Col, Typography, Button, Anchor} from 'antd';
+import { Icon, Layout, Menu, Row, Col, Typography, Button, Divider} from 'antd';
 import {Link} from 'react-router-dom';
-import RegisterDrawer from '../components/RegisterDrawer'; 
+import { Link as ScrollLink, Events, scrollSpy} from 'react-scroll'
 
+import RegisterDrawer from '../components/RegisterDrawer'; 
 import Logo from '../andres-logo.svg';
+import DentalFilling from '../dental-filling.svg';
+import Cleaning from '../cleaning.svg';
+import Whitening from '../whitening.svg';
 
 
 const { Header, Content, Footer} = Layout;
-const {Title} = Typography;
+const {Title, Text} = Typography;
 
 class HomePage extends React.Component {
+
+
+   componentDidMount() {
+      Events.scrollEvent.register('begin', function(to, element) {
+         console.log("begin", arguments);
+       });
+    
+       Events.scrollEvent.register('end', function(to, element) {
+         console.log("end", arguments);
+       });
+    
+       scrollSpy.update();
+   }
+
+   componentWillUnmount() {
+      Events.scrollEvent.remove('begin');
+      Events.scrollEvent.remove('end');
+   }
+
    render() {
       return (
         <React.Fragment>
@@ -17,7 +40,7 @@ class HomePage extends React.Component {
                <Header style={{background: '#001529'}}>
                   <Row>
                      <Col span={12}>
-                        <img height="50" src={Logo} />
+                        <img style={{height: '100%', maxHeight: '50px'}} src={Logo} />
                         <h1 style={{display: 'inline-block', margin: 0}}>
                            <span style={{color: '#0050b3'}}>Andres</span> 
                            <span style={{color: '#fff'}}> Dental</span>
@@ -50,62 +73,92 @@ class HomePage extends React.Component {
                            {/* <Button type="primary">My Patient Account</Button>
                            <a href="http://localhost:3000/" target="_blank">Link to System</a> */}
                            <Button style={{borderRadius: 10}} ghost>
-                              Learn More
+                              <ScrollLink activeClass="active" to="learnmore" spy={true} smooth={true} offset={50} duration={500}>
+                                Learn More
+                              </ScrollLink>
                            </Button>
                         
                         </Col>
                      </Row>
                   </div>
-                  <div style={{ background: '#fff', padding: 24, marginTop: 12}}>
-                     <Row>
-                           <Col span={24}>
-                              <Card
-                                 title="Default size card"
-                                 extra={<a href="#">More</a>}
-                              >
-                                 <p>Card content</p>
-                                 <p>Card content</p>
-                                 <p>Card content</p>
-                              </Card>
+                  <div style={{ background: '#fff', padding: '30px 24px'}}>
+                     <Row id="learnmore" style={{textAlign: 'center', paddingBottom: '1em'}}>
+                           <Col style={{padding: '0px 16px', marginTop: '3em'}} xs={24} md={8}>
+                              <img style={{width: '100%', maxWidth: '100px'}} src={DentalFilling}></img>
+                              <Title style={{marginTop: 35}} level={4}>Dental Filling</Title>
+                              <Text>
+                                 A dental restoration to restore the function, integrity, and morphology of missing tooth structure resulting from caries or external trauma.
+                           </Text>
                            </Col>
-                           <Col span={24}>
-                              <Card
-                                 title="Default size card"
-                                 extra={<a href="#">More</a>}
-                              >
-                                 <p>Card content</p>
-                                 <p>Card content</p>
-                                 <p>Card content</p>
-                              </Card>
+                           <Col style={{padding: '0px 16px', marginTop: '3em'}} xs={24} md={8}>
+                              <img style={{width: '100%', maxWidth: '100px'}} src={Cleaning}></img>
+                              <Title style={{marginTop: 30}} level={4}>Teeth Cleaning</Title>
+                              <Text>
+                                 Teeth Cleaning is part of oral hygiene and involves the removal of dental plaque from teeth (dental caries).
+                              </Text>
                            </Col>
-                           <Col span={24}>
-                              <Card
-                                 title="Default size card"
-                                 extra={<a href="#">More</a>}
-                              >
-                                 <p>Card content</p>
-                                 <p>Card content</p>
-                                 <p>Card content</p>
-                              </Card>
-                           </Col>
-                           <Col span={24}>
-                              <Card
-                                 title="Default size card"
-                                 extra={<a href="#">More</a>}
-                              >
-                                 <p>Card content</p>
-                                 <p>Card content</p>
-                                 <p>Card content</p>
-                              </Card>
+                           <Col style={{padding: '0px 16px', marginTop: '3em'}} xs={24} md={8}>
+                              <img style={{width: '100%', maxWidth: '100px'}} src={Whitening}></img>
+                              <Title style={{marginTop: 30}} level={4}>Teeth Whitening</Title>
+                              <Text>
+                                 Teeth Whitening is among the most popular dental procedures because it can greatly improve how your teeth look.
+                              </Text>
                            </Col>
                      </Row>
-                  
-                     
                   </div>
+                  <div id="working-photo-strip"></div>
+                  <div style={{ background: '#fff', padding: '30px 24px'}}>
+                     <div style={{textAlign: 'center'}}>
+                        <Title level={2}>How can we help</Title>
+                        <Text strong>We offer wide range of procedures to help you get that perfect smile. Visit us today!</Text>
+                     </div>
+                     <Row style={{padding: '1.5em 3em'}} gutter={100}>
+                        <Col sm={24} md={12}>
+                           <Divider orientation="left"><Text strong>ANDRES DENTAL CLINIC</Text></Divider>
+                           <Row style={{paddingLeft: '3em'}}>
+                              <Col style={{marginBottom: '1em'}}> 
+                                 <Text>
+                                    <Icon type="home" /> Inside One.O.5ive Department Store J. P. Rizal Street, Laoag City
+                                 </Text>
+                              </Col>
+                              <Col style={{marginBottom: '1em'}}>
+                                 <Text>
+                                    <Icon type="phone" /> 8888-8888-888
+                                 </Text>
+                              </Col>
 
-                           
+                              <Col style={{marginBottom: '1em'}}>
+                                 <Text>
+                                    <Icon type="mail" /> andresdentalclinic@gmail.com
+                                 </Text>
+                              </Col>
+                           </Row>
+                        </Col>
+                        <Col sm={24} md={12}>
+                           <Divider orientation="left"><Text strong>OPENING HOURS</Text></Divider>
+                           <Row style={{paddingLeft: '3em'}}>
+                              <Col style={{marginBottom: '1em'}}> 
+                                 <Text>
+                                    <Icon type="calendar" /> Monday - Friday: <Text strong>9am-6pm</Text>
+                                 </Text>
+                              </Col>
+                              <Col style={{marginBottom: '1em'}}> 
+                                 <Text>
+                                    <Icon type="calendar" /> Saturday: <Text strong>10am-4pm</Text>
+                                 </Text>
+                              </Col>
+                              <Col style={{marginBottom: '1em'}}> 
+                                 <Text>
+                                    <Icon type="calendar" /> Sunday: <Text strong>Not Available</Text>
+                                 </Text>
+                              </Col>
+                            
+                           </Row>
+                        </Col>
+                     </Row>
+                  </div>    
                </Content>
-               <Footer style={{ textAlign: 'center' }}>
+               <Footer style={{ textAlign: 'center', background: '#001529', color: '#fff' }}>
                   Andres Dental Clinic © 2019 Created By DWCL IT Group 3 - Capstone Project
                </Footer>
             </Layout>
